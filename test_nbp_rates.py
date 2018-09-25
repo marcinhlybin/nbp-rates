f1456beade06073294e60c5661fb1256670a05b3
Marcin Hlybin <marcin.hlybin@gmail.com>
2018-09-25 11:47:22 +0200

    Test for current date

--- test_nbp_rates.py
+++ test_nbp_rates.py
@@ -18,12 +18,17 @@ class UnitTests(unittest.TestCase):
         dt = datetime.strptime('2018-01-01', '%Y-%m-%d')
         self.assertEqual(nbp_rate_last('CAD', dt), ('20171229', 2.7765))
 
+    def test_nbp_rate_last_now(self):
+        dt = datetime.utcnow()
+        self.assertIsInstance(nbp_rate_last('EUR', dt)[1], float)
+
     def test_nbp_rate_last_exc(self):
         dt = datetime.strptime('2001-10-01', '%Y-%m-%d')
         self.assertRaises(ValueError, lambda: nbp_rate_last('THB', dt))
 
     def test_nbp_rates(self):
         self.assertEqual(len(list(nbp_rates('USD', 2017))), 251)
+
 
 if __name__ == '__main__':
     unittest.main()
