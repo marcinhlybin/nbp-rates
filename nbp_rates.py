0e8314baf0d59083fc9bd8c395117456c02a2eb6
Marcin Hlybin <marcin.hlybin@gmail.com>
2020-05-30 12:14:47 +0200

    Refactor into small-sized meaningful methods

--- nbp_rates.py
+++ nbp_rates.py
@@ -7,153 +7,197 @@ import csv
 import argparse
 import requests
 from datetime import datetime, timedelta
+from dataclasses import dataclass
 
 __all__ = [ 'nbp_rate_last', 'nbp_rate', 'nbp_rates' ]
 
-CACHE_FILE = '/tmp/nbp_rates_{type}_{year}.csv'
-RATES = {}
+DATA = {}
+DATA_FILE = '/tmp/nbp_rates_{type}_{year}.csv'
 
 
-def nbp_rate_last(currency, dt=datetime.utcnow()):
-    """Returns NBP currency rate for last workday
+def nbp_rate_last(symbol, dt=datetime.utcnow()):
+    """Returns NBP symbol rate for last workday
 
         Arguments:
         dt       -- date as datetime object
-        currency -- 3-letter currency symbol, e.g. EUR
+        symbol -- 3-letter currency symbol, e.g. EUR
 
         Returns a tuple (date, price)
     """
 
-    currency = currency.upper()
+    symbol = symbol.upper()
 
     for i in range(10):
         lastday = dt - timedelta(days=i)
         year = int(lastday.strftime('%Y'))
-        __init_rates(year)
+        init_rates(year)
         try:
             date = lastday.strftime('%Y%m%d')
-            return date, RATES[year][currency][date]
+            price = DATA[year][symbol][date]
+            return date, price
         except KeyError:
             if i < 9: continue
             raise
 
-def nbp_rate(currency, dt=datetime.utcnow()):
-    """Returns NBP currency rate for date
+
+def nbp_rate(symbol, dt=datetime.utcnow()):
+    """Returns NBP symbol rate for date
 
     Arguments:
     dt       -- date as datetime object
-    currency -- 3-letter currency symbol, e.g. EUR
+    symbol -- 3-letter currency symbol, e.g. EUR
 
     Returns a tuple (date, price)
     """
 
-    currency = currency.upper()
+    symbol = symbol.upper()
     year = int(dt.strftime('%Y'))
-    __init_rates(year)
+    init_rates(year)
 
     try:
         date = dt.strftime('%Y%m%d')
-        return date, RATES[year][currency][date]
+        price = DATA[year][symbol][date]
+        return date, price
     except KeyError as e:
         raise KeyError('NBP rate not found for date {}'.format(date)) from e
 
 
-def nbp_rates(currency, year):
+def nbp_rates(symbol, year):
     """Returns yearly rates for a currency
 
     Arguments:
     year     -- year string, e.g. 2018
-    currency -- 3-letter currency symbol, e.g. EUR
+    symbol -- 3-letter currency symbol, e.g. EUR
 
     Returns a generator of tuples (date, price)
     """
 
-    currency = currency.upper()
+    symbol = symbol.upper()
     year = int(year)
-    __init_rates(year)
-    for date in sorted(RATES[year][currency]):
-        yield (date, RATES[year][currency][date])
+    init_rates(year)
+    for date in sorted(DATA[year][symbol]):
+        price = DATA[year][symbol][date]
+        yield (date, price)
 
-
-def __init_rates(year):
-    current_year = int(datetime.utcnow().strftime('%Y'))
-    if year < 2002 or year > current_year:
-        raise ValueError('Rates not found for year {}'.format(year))
-
-    if year in RATES:
-        return
-
-    for rates_type in ('a', 'b'):
-        download_rates(year, rates_type)
-        cache_file = CACHE_FILE.format(type=rates_type, year=year)
-        with open(cache_file, encoding='iso8859-2') as f:
-            data = csv.reader(f, delimiter=';')
-            if year not in RATES:
-                RATES[year] = {}
-            RATES[year].update(__parse_rates(data))
-
-
-def download_rates(year, rates_type):
-    download_url = "https://www.nbp.pl/kursy/Archiwum/archiwum_tab_{type}_{year}.csv".format(
-      type=rates_type,
-      year=year
-    )
-    cache_file = CACHE_FILE.format(type=rates_type, year=year)
-    if not cache_file_is_fresh(cache_file):
-        refresh_cache_file(cache_file, download_url)
-
-def cache_file_is_fresh(cache_file):
-    if os.path.exists(cache_file):
-        curtime = int(datetime.utcnow().strftime('%s'))
-        mtime = int(os.path.getmtime(cache_file))
-        if curtime - mtime <= 3600:
-            return True
-    return False
-
-def refresh_cache_file(cache_file, download_url):
-    r = requests.get(download_url)
-    with open(cache_file, 'wb') as f:
-        f.write(r.content)
-
-
-def __parse_rates(data):
-    rates = {}
-    currencies = []
-    for row in data:
-        # Get currencies names
-        if not currencies:
-            for field in row:
-                currency_match = re.match('^\d+(\w+)$', field)
-                if currency_match:
-                    currency = currency_match[1]
-                    rates[currency] = {}
-                    currencies.append(currency)
-            continue
-
-        if not row:
-            break
-
-        # Get currency price by date
-        date = row[0]
-        date_match = re.match('^\d{8}$', date)
-        if date_match:
-            for i, currency in enumerate(currencies, 1):
-                price = row[i].replace(',', '.')
-                if not price:
-                    continue
-                rates[currency][date] = float(price)
-
-    return rates
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-y', '--year', metavar='YEAR', action='store')
-    parser.add_argument('currency', metavar='CURRENCY', type=str)
+    parser.add_argument('symbol', metavar='CURRENCY SYMBOL', type=str)
     args = parser.parse_args()
 
     if not args.year:
-        date, price = nbp_rate_last(args.currency)
+        date, price = nbp_rate_last(args.symbol)
         print("{date}\t{price}".format(date=date, price=price))
     else:
-        for date, price in nbp_rates(args.currency, args.year):
+        for date, price in nbp_rates(args.symbol, args.year):
             print("{date}\t{price}".format(date=date, price=price))
+
+
+#
+# INTERNAL IMPLEMENTATION STARTS HERE
+#
+@dataclass
+class Currency:
+    name: str
+    amount: int 
+
+
+def init_rates(year):
+    year = int(year)
+    if not is_year_available(year):
+        read_year_from_data_file(year)
+
+
+def is_year_available(year):
+    current_year = int(datetime.utcnow().strftime('%Y'))
+    if year < 2002 or year > current_year:
+        raise ValueError('Rates not found for year {}'.format(year))
+
+    if year in DATA:
+        return True
+
+
+def read_year_from_data_file(year):
+    for rates_type in ["a", "b"]:
+        data_file = DATA_FILE.format(type=rates_type, year=year)
+        
+        if not data_file_is_fresh(data_file):
+            download_rates_data(year, rates_type)
+        
+        with open(data_file, encoding='iso8859-2') as f:
+            data = csv.reader(f, delimiter=';')
+            get_year_from_data(year, data)
+
+
+def get_year_from_data(year, data):
+    if year not in DATA:
+        DATA[year] = {}
+    DATA[year].update(parse_rates_data(data))
+    
+
+def download_rates_data(year, rates_type):
+    data_file = DATA_FILE.format(type=rates_type, year=year)
+    download_url = "https://www.nbp.pl/kursy/Archiwum/archiwum_tab_{type}_{year}.csv".format(
+      type=rates_type,
+      year=year
+    )
+    r = requests.get(download_url)
+    with open(data_file, 'wb') as f:
+        f.write(r.content)
+
+
+def data_file_is_fresh(data_file):
+    if os.path.exists(data_file):
+        curtime = int(datetime.utcnow().strftime('%s'))
+        mtime = int(os.path.getmtime(data_file))
+        if curtime - mtime <= 3600:
+            return True
+
+
+def parse_rates_data(data):
+    rates = {}
+    
+    currency_row = next(data)
+    currencies = list(get_currencies(currency_row))
+    
+    for row in data:
+        if not row:
+            continue
+
+        date = row[0]
+        if not is_date(date):
+            continue
+
+        # Get currency price by date
+        for i, currency in enumerate(currencies, 1):
+            price = row[i].replace(',', '.') 
+            if not price:
+                continue
+            if currency.name not in rates:
+                rates[currency.name] = {}
+            rates[currency.name][date] = float(price)
+            currency.price = 0
+
+    return rates
+
+
+def is_date(string):
+    return re.match('^\d{8}$', string)
+
+
+def is_currency_row(row):
+    if len(row) > 0:
+        return row[0] == "data"
+
+
+def get_currencies(row):
+    if not is_currency_row(row):
+        raise ValueError("Currency row not found in data")
+    
+    for col in row:
+        currency_match = re.match('^(\d+)(\w+)$', col)
+        if currency_match:
+            yield Currency(name=currency_match[2], amount=currency_match[1])
+
+
